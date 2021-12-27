ffc957fe37fb92270e5c588d2fd7e2de9675093c
Kowalsky429 <kacper.kowalski007@onet.pl>
2021-12-27 23:41:06 +0100

    users-list-component v1

---
+++ src/data/users.js
@@ -0,0 +1,41 @@
+export const users = [
+  {
+    name: 'Adam Romanski',
+    attendace: '38%',
+    average: '2.3',
+  },
+  {
+    name: 'Krzysztof Batko',
+    attendace: '56%',
+    average: '3',
+  },
+  {
+    name: 'Patrycja Gonciarz',
+    attendace: '62%',
+    average: '3.5',
+  },
+  {
+    name: 'Olga Hahn',
+    attendace: '25%',
+    average: '2.0',
+  },
+  {
+    name: 'Pawel Andrzejowski',
+    attendace: '73%',
+    average: '3.2',
+  },
+  {
+    name: 'Szymon Knuth',
+    attendace: '41%',
+    average: '1.8',
+  },
+  {
+    name: 'Krystian Foczpaniak',
+    attendace: '71%',
+    average: '5',
+  },
+  {
+    name: 'Pawel Roman',
+    average: '1.2',
+  },
+];
