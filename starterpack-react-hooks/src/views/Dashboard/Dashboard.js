5582223eb8d96b5363ecaa09536400932467ad72
Kowalsky429 <kacper.kowalski007@onet.pl>
2022-03-20 00:17:13 +0100

    po 17 odcinku

--- starterpack-react-hooks/src/views/Dashboard/Dashboard.js
+++ starterpack-react-hooks/src/views/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, NavLink, useParams } from 'react-router-dom';
 import StudentsList from 'components/organisms/StudentsList/StudentsList';
 import { Nav, NavTitle, NavLinkItem } from './Dashboard.styles';
 import { useStudents } from 'hooks/useStudents';
 import useModal from 'components/organisms/Modal/useModal';
 import StudentDetails from 'components/molecules/StudentDetails/StudentDetails';
 import Modal from 'components/organisms/Modal/Modal';
+import { Wrapper } from 'views/Root.styles';
+import { TitleWrapper } from 'components/templates/NewsSection/NewsSection.styles';
+import { Title } from 'components/atoms/Title/Title';
+// import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   const [groups, setGroups] = useState([]);
@@ -28,27 +32,23 @@ const Dashboard = () => {
     handleOpenModal();
   };
 
-  if (!id && groups.length > 0) return <Navigate to={`/group/${groups[0]}`} />;
+  if (!id && groups.length > 0) return <Navigate to={`/group/${groups[0].id}`} />;
 
   return (
-    <>
-      <ViewWrapper>
-        <Nav>
-          <NavTitle>
-            Group <span>{id}</span>
-          </NavTitle>
-          {groups.map((group) => (
-            <NavLinkItem key={group} to={`/group/${group}`}>
-              {group}
+    <ViewWrapper>
+      <Nav>
+        <NavTitle as="h2">Group {id}</NavTitle>
+          {groups.map(({ id }) => (
+            <NavLinkItem key={id} to={`/group/${id}`}>
+              {id}{' '}
             </NavLinkItem>
           ))}
-        </Nav>
-        <StudentsList handleOpenStudentDetails={handleOpenStudentDetails} />
+      </Nav>
+      <StudentsList handleOpenStudentDetails={handleOpenStudentDetails} />
         <Modal isOpen={isOpen} handleClose={handleCloseModal}>
           <StudentDetails student={currentStudent} />
         </Modal>
-      </ViewWrapper>
-    </>
+    </ViewWrapper>
   );
 };
 
