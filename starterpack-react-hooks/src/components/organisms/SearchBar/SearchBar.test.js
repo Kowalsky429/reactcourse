7f1032c5434524eb6a6cd0117329d74b14860a38
Kowalsky429 <kacper.kowalski007@onet.pl>
2022-04-08 00:08:53 +0200

    odc 20

--- starterpack-react-hooks/src/components/organisms/SearchBar/SearchBar.test.js
+++ starterpack-react-hooks/src/components/organisms/SearchBar/SearchBar.test.js
@@ -1,21 +1,13 @@
 import { render, screen, fireEvent } from 'test-utils';
-import { setupServer } from 'msw/node';
-import { handlers } from 'mocks/handlers';
 import { SearchBar } from './SearchBar';
 
-const server = setupServer(...handlers);
-
 describe('Search Bar', () => {
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
-
   it('Renders the component', async () => {
     render(<SearchBar />);
     screen.getByText('Teacher');
     screen.getByPlaceholderText('search');
-    fireEvent.change(input, { target: { value: 'ad' } });
-    await screen.findByText(/Adam Romański/);
+    fireEvent.change(input, { target: { value: 'Low' } });
+    await screen.findByText(/Lowell/);
   });
   it('Dissapear when input is empty', () => {
     fireEvent.change(input, { target: { value: '' } });
